e18e24156578494e675f05c7cddba6f80242792a
Lucas Lopes <Lucas.santos.pessoal@outlook.com>
2021-12-29 10:27:11 -0300

    Corrigindo o bug de colocar outro simbolo no card em que já foi colocado o simbolo

--- scripts/game.js
+++ scripts/game.js
@@ -4,12 +4,14 @@ let symbols = ['o', 'x'];
 
 function handleMove(position) {
 
-    board[position] = symbols[playerTime];
+    if(board[position] == ''){
+        board[position] = symbols[playerTime];
 
-    if(playerTime == 0){
-        playerTime = 1;
-    }else{
-        playerTime = 0;
+        if(playerTime == 0){
+            playerTime = 1;
+        }else{
+            playerTime = 0;
+        }
     }
-
+    
 }
