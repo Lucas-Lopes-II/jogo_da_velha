41e92701376a37fd122941247117bbb58dff768d
Lucas Lopes <Lucas.santos.pessoal@outlook.com>
2021-12-29 10:23:54 -0300

    Criando a função de colocar simbolo quando cliocar no card

--- scripts/interface.js
+++ scripts/interface.js
@@ -10,6 +10,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function handleClick(event) {
 
-    console.log(event.target);
+    let square = event.target;
+    let position = square.id;
+
+    handleMove(position);
+    updateSquares()  
+}
+
+function updateSquares(){
+
+    let squares = document.querySelectorAll('.square');
+
+    squares.forEach((square) => {
+        let position = square.id;
+        let symbol = board[position];
+
+        if(symbol != ''){
+            square.innerHTML = `<div class='${symbol}'></div>`
+        }
+    })
 
 }
